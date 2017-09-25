80b0931c064a121b638281b3742a9e734a201690
ic_106024033 <yiluntzeng@gmail.com>
2017-09-25 14:32:23 +0800

    sjjd

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("106024033曾翊倫");// write your code here
+        System.out.print("資傳1A");
+    }
+}
